86e520ff9cf7327b86eae5785d8203afb2549084
Rafael Carvalho <rafaelcrvs@gmail.com>
2017-03-23 23:01:52 -0300

    Initial Commit

---
+++ Generator Numbers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Generator Numbers
+//
+//  Created by Rafael Carvalho on 23/03/17.
+//  Copyright © 2017 Rafael Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
