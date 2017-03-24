4b2d806c939cd7f1801507e40fac24c61be78118
Rafael Carvalho <rafaelcrvs@gmail.com>
2017-03-24 00:37:59 -0300

    First commit
    Publishing the app to generate random numbers from 0 to 10

--- Generator Numbers/ViewController.swift
+++ Generator Numbers/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var resultLegend: UILabel!
+    
+    @IBAction func generateNumber(_ sender: Any) {
+        
+        var randomNumber = arc4random_uniform(11)
+        
+        resultLegend.text = String(randomNumber)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
